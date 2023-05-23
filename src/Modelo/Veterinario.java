91d009cd25785ca412af3c58ff1a8b194fd2b095
AlexaReyE <alexandrareyescobar@gmail.com>
2023-05-22 22:28:24 -0500

    Modelo actualizado

---
+++ src/Modelo/Veterinario.java
@@ -0,0 +1,32 @@
+package Modelo;
+
+import java.util.ArrayList;
+
+public class Veterinario {
+	private String nombre;
+	private String correo;
+	private String telefono;
+	private static ArrayList<Veterinario> listaVeterinarios  = new ArrayList<Veterinario>();
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	public static Veterinario encontrarPorId
+	
+	
+}
